e90f5574438337389fdbaf08d040750a93e7f5ea
Jae Hui Seo <dayjaycs49@gmail.com>
2021-07-22 23:12:19 -0400

    tailwindcss setup and apollo graphql setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "./styles/styles.css";
+import { client } from "./apollo";
+import { ApolloProvider } from '@apollo/client';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
