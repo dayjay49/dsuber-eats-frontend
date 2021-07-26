350f92495bfa6d2c5a671215ef590deb23262e20
Jae Hui Seo <dayjaycs49@gmail.com>
2021-07-26 11:38:33 -0400

    Form Login with useForm()

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,20 +1,59 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+
+interface ILoginForm {
+  email?: string;
+  password?: string;
+}
 
 export const Login = () => {
+  const { register, getValues, handleSubmit, formState: { errors } } = useForm<ILoginForm>();
+  const onValidSubmit = () => {
+    console.log(getValues());
+  };
   return (
     <div className="h-screen flex items-center justify-center bg-gray-800">
-      <div className="bg-white w-full max-w-lg px-10 py-10 rounded-lg text-center">
+      <div className="bg-white w-full max-w-lg px-6 pt-6 pb-6 rounded-lg text-center">
         <h3 className="text-3xl text-gray-800">Login</h3>
-        <form className="flex flex-col mt-5 px-5">
-          <input
-            placeholder="Email"
-            className="bg-gray-100 shadow-inner focus:outline-none border focus:border-opacity-50 focus:border-green-600 mb-2 py-3 px-5 rounded-lg"/>
-          <input
-            placeholder="Password"
-            className="bg-gray-100 shadow-inner focus:outline-none border focus:border-opacity-50 focus:border-green-600  py-3 px-5 rounded-lg"/>
-          <button className="mt-2 py-3 px-5 rounded-lg bg-gray-800 text-white text-lg focus:outline-none hover:opacity-90">
-            Log In
-          </button>
+        <form 
+          onSubmit={handleSubmit(onValidSubmit)}
+          className="grid gap-3 mt-5 px-5"
+        >
+          <input 
+            {...register('email', {
+              required: "Email is required",
+            })} 
+            name="email"
+            required
+            type="email"
+            placeholder="Email" className="mb-2 input"
+          />
+          {errors.email?.message && (
+            <span className="font-medium text-red-500">
+              {errors.email?.message}
+            </span>
+          )}
+          <input 
+            {...register('password', {
+              required: "Password is required",
+              minLength: 5,
+            })} 
+            name="password"
+            required
+            type="password"
+            placeholder="Password" className="input"
+          />
+          {errors.password?.message && (
+            <span className="font-medium text-red-500">
+              {errors.password?.message}
+            </span>
+          )}
+          {errors.password?.type === "minLength" && (
+            <span className="font-medium text-red-500">
+              Password must be more than 5 characters.
+            </span>
+          )}
+          <button className="mt-2 btn">Log In</button>
         </form>
       </div>
     </div>
