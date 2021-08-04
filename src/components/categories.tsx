eec73c4a642a2fcd8b8e843dad9655e6e9c60788
Jae Hui Seo <dayjaycs49@gmail.com>
2021-08-04 12:48:10 -0400

    Making a new route for the categories and learning to use

--- src/components/categories.tsx
+++ src/components/categories.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { restaurantsPageQuery_allCategories_categories } from "../__generated__/restaurantsPageQuery";
 
 interface ICategoriesProps {
@@ -9,14 +10,16 @@ export const Categories: React.FC<ICategoriesProps> = ({categoriesData}) => {
   return (
     <div className="flex justify-around max-w-lg mx-auto">
       {categoriesData?.map((category, i) => 
-        <div key={category.id} className="flex flex-col items-center group cursor-pointer">
-          <div 
-            className="w-16 h-16 rounded-full bg-cover group-hover:bg-gray-100" 
-            style={{backgroundImage: `url(${category.coverImg})`}}
-          >
+        <Link key={category.id} to={`/category/${category.slug}`}>
+          <div className="flex flex-col items-center group cursor-pointer">
+            <div 
+              className="w-16 h-16 rounded-full bg-cover group-hover:bg-gray-100" 
+              style={{backgroundImage: `url(${category.coverImg})`}}
+            >
+            </div>
+            <span className="mt-1 text-sm text-center font-medium">{category.name}</span>
           </div>
-          <span className="mt-1 text-sm text-center font-medium">{category.name}</span>
-        </div>
+        </Link>
       )}
     </div>
   );
